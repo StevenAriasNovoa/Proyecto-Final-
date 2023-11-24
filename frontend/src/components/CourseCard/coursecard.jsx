0f13738f5ac947eee2a9737bfd53224024cac010
DeylanObandoA <136619722+DeylanObandoA@users.noreply.github.com>
2023-11-24 15:44:50 -0600

    advance

--- frontend/src/components/CourseCard/coursecard.jsx
+++ frontend/src/components/CourseCard/coursecard.jsx
@@ -3,35 +3,37 @@ import "./coursecard.css";
 
 function CourseCard(props) {
   return (
-    <section className="courses body4 product-container product-1">
-      {props.courses &&
-        props.courses.map((course) => (
-          <div className="card" key={course.id}>
-            <div className="photo"></div>
-            <div className="content">
-              <div className="title">
-                {course.name}
+    <div>
+      <section className= "course-container courses body4 product-container product-1">
+        {props.courses &&
+          props.courses.map((course) => (
+            <div className="card" key={course.id}>
+              <div className="photo"></div>
+              <div className="content">
+                <div className="title">
+                  {course.name}
+                </div>
+                <div className="bg-title">
+                  {course.name}
+                </div>
+                <div className="feature size">
+                  <div>size :</div>
+                  <span>S</span>
+                  <span>M</span>
+                  <span>L</span>
+                </div>
+                <div className="feature color">
+                  <div>color :</div>
+                  <span>pink</span>
+                  <span className="tt">blue</span>
+                  <span className="ttt">green</span>
+                </div>
+                <button className="btn-buy">buy now</button>
               </div>
-              <div className="bg-title">
-              algo
-              </div>
-              <div className="feature size">
-                <div>size :</div>
-                <span>S</span>
-                <span>M</span>
-                <span>L</span>
-              </div>
-              <div className="feature color">
-                <div>color :</div>
-                <span>pink</span>
-                <span className="tt">blue</span>
-                <span className="ttt">green</span>
-              </div>
-              <button className="btn-buy">buy now</button>
             </div>
-          </div>
-        ))}
-    </section>
+          ))}
+      </section>
+    </div>
   );
 }
 
